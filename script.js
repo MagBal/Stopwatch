41cda187ff73df26a05eda6a93432f1412b63dd0
Magda Balun <magi@op.pl>
2018-01-16 10:48:27 +0100

    Format .js file

--- script.js
+++ script.js
@@ -64,14 +64,14 @@ class Stopwatch extends React.Component {
     render() {
         return (
             <div>
-        		<nav className='controls'>
-          			<button className='start' onClick={()=> this.start() }>Start</button>
-          			<button className='stop' onClick={()=> this.stop() }>Stop</button>
-        		</nav>
-        		<div className={'stopwatch'}>{this.format()}</div>
-      		</div>
-    	);
-  	}
+                <nav className='controls'>
+                    <button className='start' onClick={()=> this.start() }>Start</button>
+                    <button className='stop' onClick={()=> this.stop() }>Stop</button>
+                </nav>
+                <div className={'stopwatch'}>{this.format()}</div>
+            </div>
+        );
+    }
 };
 
 const stopwatch = new Stopwatch (
